0154ba7ac95f081229a2a6491058aa33dcf62e1e
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2025-03-19 11:16:36 +0200

    configured storybook

---
+++ config/storybook/main.ts
@@ -0,0 +1,75 @@
+// import type {StorybookConfig} from '@storybook/react-webpack5'
+
+import path from 'path'
+import {Configuration, DefinePlugin} from 'webpack'
+import {ConfigPaths} from '../build/configOptions'
+import {getCssLoader} from '../build/loaders/getCssLoader'
+
+const config = {
+  stories: [
+    path.resolve(__dirname, '..', '..', 'src', '**/*.mdx'),
+    path.resolve(__dirname, '..', '..', 'src', '**/*.stories.@(js|jsx|mjs|ts|tsx)'),
+  ],
+  addons: [
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-styling-webpack',
+    'storybook-css-modules',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  swc: () => ({
+    jsc: {
+      transform: {
+        react: {
+          runtime: 'automatic',
+        },
+      },
+    },
+  }),
+  webpackFinal: async (config: Configuration) => {
+    const paths: ConfigPaths = {
+      buildPath: '',
+      htmlPath: '',
+      entryPath: '',
+      srcPath: path.resolve(__dirname, '..', '..', 'src'),
+    }
+    config.resolve?.modules?.push(path.relative(__dirname, paths.srcPath), 'node_modules')
+    config.resolve?.extensions?.push('.ts', '.tsx')
+    config.module?.rules?.push(getCssLoader(true))
+    // Find and remove the existing rule for handling SVGs
+    // eslint-disable-next-line
+  const fileLoaderRule: any = config.module?.rules?.find((rule: any) => {
+      if (rule.test) {
+        return rule.test?.test('.svg')
+      }
+      return null
+    })
+    fileLoaderRule.exclude = /\.svg$/
+
+    // Add new rule for handling SVGs with @svgr/webpack
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    config.plugins?.push(new DefinePlugin({
+      __IS_DEV__: JSON.stringify(true),
+      __API__: JSON.stringify('https://test-api.ua'),
+      __PROJECT__: JSON.stringify('storybook'),
+    }))
+
+    config.plugins?.push(
+      new DefinePlugin({
+      }),
+    )
+
+    return config
+  },
+}
+export default config
