277d6e9e8468fc23b812a151dbd9f75ca2b1aba9
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2025-03-27 16:10:56 +0200

    fixed type issues with tsconfig

--- global.d.ts
+++ global.d.ts
@@ -6,6 +6,11 @@ declare module '*.scss' {
   export = classNames;
 }
 
+declare module '*.module.scss' {
+  const classes: { [key: string]: string }
+  export default classes
+}
+
 declare module '*.svg' {
   const svg: string
   export default svg
