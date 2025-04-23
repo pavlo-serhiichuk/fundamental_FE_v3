c3574534cc79063a2160fc88615629401344e37f
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2025-04-23 13:40:47 +0300

    fix imports & scss

--- src/pages/ArticlesPage/module/services/initArticlesList/initArticlesList.ts
+++ src/pages/ArticlesPage/module/services/initArticlesList/initArticlesList.ts
@@ -1,12 +1,8 @@
 import {type ThunkConfig} from 'app/providers/StoreProvider'
 import {createAsyncThunk} from '@reduxjs/toolkit'
-import {getArticlesPageNumber} from 'pages/ArticlesPage/module/selectors/getArticlesPageNumber'
 import {fetchArticlesList} from 'pages/ArticlesPage/module/services/fetchArticlesList/fetchArticlesList'
 import {articlesPageActions} from 'pages/ArticlesPage/module/slice/articlesPageSlice'
-import {getArticlesPageHasMore} from 'pages/ArticlesPage/module/selectors/getArticlesPageHasMore'
-import {getArticlesIsLoading} from 'pages/ArticlesPage/module/selectors/getArticlesIsLoading'
 import {getArticlesPageInited} from 'pages/ArticlesPage/module/selectors/getArticlesPageInited'
-import {useSelector} from 'react-redux'
 import {getListView} from 'features/ChangeListView'
 
 export const initArticlesList = createAsyncThunk<void, undefined, ThunkConfig<string>>(
