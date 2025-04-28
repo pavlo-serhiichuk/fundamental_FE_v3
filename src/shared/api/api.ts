ea95e86ae82d54fb66a9b1dbe81d7b0ce17a83de
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2025-04-28 16:58:01 +0300

    fixed api

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,8 +3,12 @@ import {AUTH_USER_DATA} from 'shared/const/localStorage'
 
 export const $api = axios.create({
   baseURL: __API__, // Use the correct base URL
-  headers: {
-    authorization: localStorage.getItem(AUTH_USER_DATA) || '',
-  },
   withCredentials: true, // Enable this if you're sending cookies or other credentials
+})
+
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.Authorization = localStorage.getItem(AUTH_USER_DATA) || ''
+  }
+  return config
 })
