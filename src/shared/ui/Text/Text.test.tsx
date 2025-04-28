0a2be370397cb5af830e290ebdc29aca139acd96
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2025-04-28 17:21:44 +0300

    fixed test v2

--- src/shared/ui/Text/Text.test.tsx
+++ src/shared/ui/Text/Text.test.tsx
@@ -5,17 +5,9 @@ import {Text} from './Text'
 // 'title-el'
 // 'text-el'
 describe('Text', () => {
-  test('exist', () => {
+  test('exist title & text', () => {
     renderTestComponent(<Text text="text" title="title" />)
     expect(screen.getByTestId('title-el')).toBeInTheDocument()
     expect(screen.getByTestId('text-el')).toBeInTheDocument()
-  })
-  test('switch', () => {
-    renderTestComponent(<Text />)
-    expect(screen.getByTestId('theme-switcher')).toHaveClass('light')
-    fireEvent.click(screen.getByTestId('theme-switcher'))
-    expect(screen.getByTestId('theme-switcher')).toHaveClass('green')
-    fireEvent.click(screen.getByTestId('theme-switcher'))
-    expect(screen.getByTestId('theme-switcher')).toHaveClass('dark')
   })
 })
